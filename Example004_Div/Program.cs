c18b2d5368f97f903a45c36836ef61f87cfa436a
Elshan <beka417@yandex.ru>
2023-04-08 15:12:30 +0300

    Seminar 1

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,12 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿Console.WriteLine("Введите первое число: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int Quad = Num2 * Num2;
+if (Quad == Num1){
+    Console.WriteLine($"число {Num1} является квадратом {Num2}");
+}
+else{
+        Console.WriteLine($"число {Num1} не является квадратом {Num2}");
+
+}
